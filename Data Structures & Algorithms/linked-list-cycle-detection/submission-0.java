cdc490e639307c4b093ce6e5e3adbb29e1155497
levelingpotato <155598629+levelingpotato@users.noreply.github.com>
2026-07-31 13:26:19 -0500

    Add: linked-list-cycle-detection - submission-0

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-0.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> nodeSet = new HashSet<>();
+
+        while(head != null){
+            if(nodeSet.contains(head)){
+                return true;
+            } else {
+                nodeSet.add(head);
+            }
+            head = head.next;
+        }
+
+        return false;
+    }
+}
